269d2de892183508e097ce3b0f0fd6a14f8695cf
rulkimi <azrulkimi00@gmail.com>
2024-11-09 20:24:26 +0800

    add header

---
+++ components/Header.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const settings = useSettings();
+</script>
+
+<template>
+  <Bounded as="header" class="!py-4 md:!py-6 lg:!py-8">
+    <div class="flex gap-4 items-center justify-between flex-col sm:flex-row">
+      <Logo />
+      <nav>
+        <ul class="flex">
+          <li
+            v-for="item of settings ?.data.navigation"
+            :key="item.label ?? ''"
+            class="p-4"
+          >
+            <PrismicLink :field="item.link">
+              {{ item.label }}
+            </PrismicLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </Bounded>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <Header />
+    <slot></slot>
+    <div class="fixed inset-0 bg-gradient-to-tr from-emerald-50 to-cyan-50 z-[-1] opacity-50"></div>
+  </div>
+</template>
